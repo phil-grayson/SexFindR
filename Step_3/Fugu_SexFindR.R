7530c86d2ecbebad1a408a4cdf36536d8be5532b
Phil Grayson <phil@Phils-iMac.local>
2021-11-11 11:26:51 -0500

    working SexFindR R code - still need python

--- Step_3/Fugu_SexFindR.R
+++ Step_3/Fugu_SexFindR.R
@@ -10,56 +10,33 @@ fugu_SNP_filter <- fugu_SNP %>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC
 fugu_SNP_filter_arrange <- fugu_SNP_filter %>% arrange(Pvalue,-abs(mean_MvF_dif)) %>% rownames_to_column() %>% rename(SNPdensity_rank=rowname) %>% mutate(SNPdensity_rank=as.numeric(SNPdensity_rank))
 
 # GWAS. grab top 5% of sites and write them out for downstream python
-fugu_gemma <- read_tsv("~/Desktop/GEMMA/Fugu/test_gemma_out.assoc.txt") %>% separate(rs, c("scaf","base"),sep=':')%>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC_004299.1")
+fugu_gemma <- read_tsv("~/SexFindR/Step_3/test_gemma_out.assoc.txt.zip") %>% separate(rs, c("scaf","base"),sep=':')%>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC_004299.1")
 cutoff <- round(nrow(fugu_gemma)*0.05)
 fugu_gemma_sort_cut <- head(fugu_gemma %>% arrange(p_lrt), n=cutoff) 
 #write_tsv(path="fugu_gemma_sort_cut.txt",fugu_gemma_sort_cut %>% select(scaf,base),col_names = F)
-gemma_window_count_fugu <- read_tsv("~/Desktop/fugu/gemma_window_count.txt") %>% arrange(-count) %>% rownames_to_column() %>% rename(GWAScount_rank=rowname) %>% mutate(GWAScount_rank = as.numeric(GWAScount_rank))
+gemma_window_count_fugu <- read_tsv("~/SexFindR/Step_3/gemma_window_count.txt") %>% arrange(-count) %>% rownames_to_column() %>% rename(GWAScount_rank=rowname) %>% mutate(GWAScount_rank = as.numeric(GWAScount_rank))
 
 # combine SNP density and GWAS 
 SNP_gemma_fugu <- full_join(fugu_SNP_filter_arrange,gemma_window_count_fugu) %>% select(scaf,base,GWAScount_rank,SNPdensity_rank)
 top_SNP_gemma_fugu <- SNP_gemma_fugu %>% filter(GWAScount_rank<=100,SNPdensity_rank<=100)
 
 # now we want Fst
-Fstugu <- read_tsv("~/Desktop/fugu/biallelic_fst.weir.fst") %>% replace_na(list(WEIR_AND_COCKERHAM_FST=0)) %>% rename(scaf = CHROM, base=POS) %>% mutate(base=as.numeric(base)) %>% filter(WEIR_AND_COCKERHAM_FST >=0) %>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC_004299.1")
+Fstugu <- read_tsv("~/SexFindR/Step_3/biallelic_fst.weir.fst") %>% replace_na(list(WEIR_AND_COCKERHAM_FST=0)) %>% rename(scaf = CHROM, base=POS) %>% mutate(base=as.numeric(base)) %>% filter(WEIR_AND_COCKERHAM_FST >=0) %>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC_004299.1")
 cutoff <- round(nrow(Fstugu)*0.05)
 Fstugu_sort_cut <- head(Fstugu %>% arrange(-WEIR_AND_COCKERHAM_FST), n=cutoff) 
 #write_tsv(path="Fstugu_sort_cut.txt",Fstugu_sort_cut %>% select(scaf,base),col_names = F)
 
-Fstugu_window_count <- read_tsv("~/Desktop/fugu/Fstugu_window_count.txt") %>% arrange(-count) %>% rownames_to_column() %>% rename(Fstcount_rank=rowname) %>% mutate(Fstcount_rank = as.numeric(Fstcount_rank))
+Fstugu_window_count <- read_tsv("~/SexFindR/Step_3/Fstugu_window_count.txt") %>% arrange(-count) %>% rownames_to_column() %>% rename(Fstcount_rank=rowname) %>% mutate(Fstcount_rank = as.numeric(Fstcount_rank))
 
 # combine SNP_gemma with Fstugu_window_count
 all_SNP_based_fugu <- full_join(SNP_gemma_fugu,Fstugu_window_count %>% select(-count)) %>% mutate(total_rank = GWAScount_rank+SNPdensity_rank+Fstcount_rank)
 top_SNP_based_fugu <- all_SNP_based_fugu %>% filter(GWAScount_rank<=100,SNPdensity_rank<=100,Fstcount_rank <=100) %>% mutate(total_rank = GWAScount_rank+SNPdensity_rank+Fstcount_rank)
 #top_SNP_based_fugu %>% select(-total_rank) %>% write_tsv("Fugu_SexFindR_candidate_windows.txt")
 
-# finally, the permuted depth
-depth_fugu <- read_tsv("depth_p005_window_count.txt") %>% arrange(-count) %>% rownames_to_column() %>% rename(Depthcount_rank=rowname) %>% mutate(Depthcount_rank = as.numeric(Depthcount_rank)) %>% select(-count)
-
-# all together now!
-all_data_fugu <- full_join(all_SNP_based_fugu,depth_fugu)
-top_all_data_fugu <- all_data_fugu %>% filter(GWAScount_rank<=100,SNPdensity_rank<=100,Fstcount_rank <=100,Depthcount_rank<=10000)
-
-top_all_data_fugu <- all_data_fugu %>% filter(GWAScount_rank<=100,SNPdensity_rank<=100,Fstcount_rank <=100,Depthcount_rank<=10000)
-
-# some correlation tests:
-# so top_all_data has nothing of note out to 10k....let's look at correlations:
-cor.test(all_data_fugu$GWAScount_rank,all_data_fugu$SNPdensity_rank)
-#all_data %>% ggplot(aes(x=GWAScount_rank,y=SNPdensity_rank)) + geom_point()
-cor.test(all_data_fugu$GWAScount_rank,all_data_fugu$Fstcount_rank)
-all_data_fugu %>% ggplot(aes(x=GWAScount_rank,y=Fstcount_rank)) + geom_point()
-cor.test(all_data_fugu$SNPdensity_rank,all_data_fugu$Fstcount_rank)
-#all_data %>% ggplot(aes(x=SNPdensity_rank,y=Fstcount_rank)) + geom_point()
-# not correlated
-cor.test(all_data_fugu$Depthcount_rank,all_data_fugu$Fstcount_rank)
-cor.test(all_data_fugu$Depthcount_rank,all_data_fugu$SNPdensity_rank)
-cor.test(all_data_fugu$Depthcount_rank,all_data_fugu$GWAScount_rank)
-
-
 # want to make a plot for the top region
 
 ##### --- Fugu only NC_042303.1 --- #####
-scaffold_lengths <- read_tsv("~/Desktop/fugu/GCF_901000725.2_fTakRub1.2_genomic.fna.fai", col_names = c("scaf","length")) %>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC_004299.1") 
+scaffold_lengths <- read_tsv("~/SexFindR/Step_3/GCF_901000725.2_fTakRub1.2_genomic.fna.fai", col_names = c("scaf","length")) %>% filter(grepl("NC_",scaf)) %>% filter(scaf != "NC_004299.1") 
 # 95% and 99% outliers
 #quantile(Fstugu$`WEIR_AND_COCKERHAM_FST`, c(0.95, 0.99), na.rm = T)
 #test <- left_join(scaffold_lengths,Fstugu) %>% filter(WEIR_AND_COCKERHAM_FST>=0) %>% filter(scaf == "NC_042303.1")
@@ -73,14 +50,12 @@ fugu_fst_count <- left_join(scaffold_lengths,Fstugu_window_count) %>% filter(sca
 
 a <- fugu_fst_count %>% ggplot(aes(x=base, y=count, color=count)) + geom_point() + theme(axis.text.x = element_text(angle = 90)) + labs(x="",color="") + labs(y="Fst (Weir and Cockerham) outliers per 10k window",color="") + geom_vline(xintercept = 12710000,linetype="dotted",color="red",size=1) + labs(title = "A. Fst")+ geom_vline(xintercept = 9270000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 2990000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 9290000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 10600000,linetype="dotted",color="black",size=0.5) +
   scale_x_continuous(n.breaks = 10,labels = scales::comma, limits = c(0,x_length$length), expand = c(0, 0)) +
-  scale_y_continuous(limits = c(0,max(test2$count)*1.05), expand = c(0, 0)) + theme_igray() + scale_color_gradient(low = "#e5e5e5",high = "#0072B2") +theme(legend.position = "none")
+  scale_y_continuous(limits = c(0,max(fugu_fst_count$count)*1.05), expand = c(0, 0)) + theme_igray() + scale_color_gradient(low = "#e5e5e5",high = "#0072B2") +theme(legend.position = "none")
 
-#color="mediumpurple"
 mid=0
 b <- fugu_SNP%>% filter(scaf == "NC_042303.1") %>% ggplot(aes(x=base,y=mean_MvF_dif,color=mean_MvF_dif)) +geom_point()+ geom_vline(xintercept = 12710000,linetype="dotted",color="red",size=1)  + labs(x="",color="") + labs(y="10kb SNP Density (Male mean - Female mean)",color="")+ labs(title = "B. SNP Density")+ geom_vline(xintercept = 9270000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 2990000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 9290000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 10600000,linetype="dotted",color="black",size=0.5)+ theme_igray() + scale_x_continuous(n.breaks = 10,labels = scales::comma, limits = c(0,x_length$length), expand = c(0, 0)) + scale_color_gradient2(midpoint=mid, low = "#332288", mid = "#e5e5e5",high = "#332288") +theme(legend.position = "none")
 
-#color="orchid2"
-c <- gemma_window_count_fugu %>% filter(scaf == "NC_042303.1") %>% ggplot(aes(x=base,y=count,color=count)) +geom_point()+ geom_vline(xintercept = 12710000,linetype="dotted",color="red",size=1)  + labs(x="Position (bp on fugu NC_042303.1)",color="") + labs(y="GWAS outliers per 10kb window",color="") + labs(title = "C. Gemma GWAS") + geom_vline(xintercept = 9270000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 2990000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 9290000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 10600000,linetype="dotted",color="black",size=0.5)+ theme_igray() +scale_x_continuous(n.breaks = 10,labels = scales::comma, limits = c(0,x_length$length), expand = c(0, 0))+ scale_color_gradient(low = "#e5e5e5",high = "#882255") +theme(legend.position = "none")
+c <- gemma_window_count_fugu %>% filter(scaf == "NC_042303.1") %>% ggplot(aes(x=base,y=count,color=count)) +geom_point()+ geom_vline(xintercept = 12710000,linetype="dotted",color="red",size=1)  + labs(x="Position (bp) on fugu NC_042303.1",color="") + labs(y="GWAS outliers per 10kb window",color="") + labs(title = "C. Gemma GWAS") + geom_vline(xintercept = 9270000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 2990000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 9290000,linetype="dotted",color="black",size=0.5) + geom_vline(xintercept = 10600000,linetype="dotted",color="black",size=0.5)+ theme_igray() +scale_x_continuous(n.breaks = 10,labels = scales::comma, limits = c(0,x_length$length), expand = c(0, 0))+ scale_color_gradient(low = "#e5e5e5",high = "#882255") +theme(legend.position = "none")
 
 a/b/c
-#ggsave("NC_042303_fugu_sexFindR_results.pdf")
+#ggsave("NC_042303_fugu_sexFindR_results_figure.pdf")
