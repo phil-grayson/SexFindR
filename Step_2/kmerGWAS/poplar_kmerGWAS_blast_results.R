87654fd3f6a3fe1f94c6380951529dca5921e95e
phil-grayson <phil@Phils-iMac.cable.rcn.com>
2022-02-14 08:35:12 -0500

    update poplar blast figure

--- Step_2/kmerGWAS/poplar_kmerGWAS_blast_results.R
+++ Step_2/kmerGWAS/poplar_kmerGWAS_blast_results.R
@@ -10,6 +10,7 @@ poplar_kmerV2_filter <- poplar_kmerV2 %>% group_by(X1) %>% top_n(n = 1, wt = -X1
 
 a<-poplar_kmerV2_filter %>% ungroup() %>% count(X2) %>% rename(`Abyss contig blastn hits`=n,Chromosome=X2) %>% ggplot(aes(x=Chromosome,y=`Abyss contig blastn hits`)) + geom_col() + theme(axis.text.x = element_text(angle = 90)) + ggtitle("A. Male-specific k-mers from kmerGWAS in poplar assembled with Abyss and linked to v2.2 of the reference genome using blastn")
 
-b<-poplar_kmerV2_filter%>% filter(X2 == "scaffold_19") %>% ggplot(aes(x=X9,y=X3)) + geom_point() + xlab('bp') + ylab('Percent identity for blastn') + scale_x_continuous(breaks=seq(0,16000000,1000000)) + ggtitle("B. Distribution of male-specific k-mers from kmerGWAS across poplar v2.2 chromosome 19 from blastn")
+b<-poplar_kmerV2_filter%>% filter(X2 == "scaffold_19") %>% ggplot(aes(x=X9,y=X3)) + geom_point() + xlab('Position on chromosome (bp)') + ylab('Percent identity for blastn') + scale_x_continuous(breaks=seq(0,16000000,1000000)) + ggtitle("B. Distribution of male-specific k-mers from kmerGWAS across poplar v2.2 chromosome 19 from blastn")
 
 a/b
+ggsave("kmersGWAS_poplar_blast.pdf", width = 11.5, height = 7)
